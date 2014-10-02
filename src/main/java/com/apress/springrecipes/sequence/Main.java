c9353e4cb50eb1366a15d8eaec5470fe640d17dc
Serge009 <serge.naumovych@gmail.com>
2014-10-02 22:08:10 +0300

    1-8

--- src/main/java/com/apress/springrecipes/sequence/Main.java
+++ src/main/java/com/apress/springrecipes/sequence/Main.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans-1.6.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans-1.8.xml");
         SequenceGenerator generator = context.getBean("sequenceGenerator", SequenceGenerator.class);
 
         System.out.println(generator.getSequence());

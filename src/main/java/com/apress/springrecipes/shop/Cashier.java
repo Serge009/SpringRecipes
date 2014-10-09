36ba290ac3559c010b5024915fa21522635ba88b
Serge009 <serge.naumovych@gmail.com>
2014-10-09 18:08:34 +0300

    2-7

---
+++ src/main/java/com/apress/springrecipes/shop/Cashier.java
@@ -0,0 +1,54 @@
+package com.apress.springrecipes.shop;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.*;
+import java.util.Date;
+
+/**
+ * Created by Matrix on 09.10.2014.
+ */
+public class Cashier implements InitializingBean, DisposableBean {
+    private String name;
+    private String path;
+    private BufferedWriter writer;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @PostConstruct
+    public void openFile() throws IOException {
+        File logFile = new File(path, name + ".txt");
+        writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(logFile, true)));
+    }
+    public void checkout(ShoppingCart cart) throws IOException {
+        double total = 0;
+        for (Product product : cart.getItems()) {
+            total += product.getPrice();
+        }
+        writer.write(new Date() + "\t" + total + "\r\n");
+        writer.flush();
+    }
+
+    @PreDestroy
+    public void closeFile() throws IOException {
+        writer.close();
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        //closeFile();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //openFile();
+    }
+}
--- src/main/java/com/apress/springrecipes/shop/Main.java
+++ src/main/java/com/apress/springrecipes/shop/Main.java
@@ -3,22 +3,25 @@ package com.apress.springrecipes.shop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by SERGE on 02.10.2014.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("2/beans-2.6.xml");
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("2/beans-2.7.xml");
         Product aaa = (Product) context.getBean("aaa");
         Product cdrw = (Product) context.getBean("cdrw");
         Product dvdrw = (Product) context.getBean("dvdrw");
-        ShoppingCart cart1 = (ShoppingCart) context.getBean("shoppingCart");
+        ShoppingCart cart1 = new ShoppingCart();
         cart1.addItem(aaa);
         cart1.addItem(cdrw);
+        cart1.addItem(dvdrw);
         System.out.println("Shopping cart 1 contains " + cart1.getItems());
-        ShoppingCart cart2 = (ShoppingCart) context.getBean("shoppingCart");
-        cart2.addItem(dvdrw);
-        System.out.println("Shopping cart 2 contains " + cart2.getItems());
+
+        Cashier cashier1 = (Cashier) context.getBean("cashier1");
+        cashier1.checkout(cart1);
     }
 }

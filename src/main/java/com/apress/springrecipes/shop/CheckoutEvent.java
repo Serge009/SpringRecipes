b214b6c518e3d1aa2d81cb2d4d8f2286ecfaa2a7
Serge009 <serge.naumovych@gmail.com>
2014-10-20 17:44:08 +0300

    add CheckoutEvent class

---
+++ src/main/java/com/apress/springrecipes/shop/CheckoutEvent.java
@@ -0,0 +1,38 @@
+package com.apress.springrecipes.shop;
+
+import org.springframework.context.ApplicationEvent;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Matrix on 20.10.2014.
+ */
+public class CheckoutEvent extends ApplicationEvent {
+
+    private double amount;
+    private Date time;
+
+    public CheckoutEvent(Object source, double amount, Date time) {
+        super(source);
+        this.amount = amount;
+        this.time = time;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+}

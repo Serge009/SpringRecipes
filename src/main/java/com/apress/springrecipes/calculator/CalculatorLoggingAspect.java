013b270436cd888e96eeb4288b47eb11eb473745
Serge009 <serge.naumovych@gmail.com>
2014-10-21 11:03:14 +0300

    3-2

---
+++ src/main/java/com/apress/springrecipes/calculator/CalculatorLoggingAspect.java
@@ -0,0 +1,76 @@
+package com.apress.springrecipes.calculator;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+/**
+ * Created by Matrix on 21.10.2014.
+ */
+@Aspect
+public class CalculatorLoggingAspect {
+    private static Logger log = LoggerFactory.getLogger(CalculatorLoggingAspect.class);
+
+    @Before("execution(* ArithmeticCalculator.add(..))")
+    public void logBefore(){
+        //log.info("{}", "The method add() begins");
+    }
+
+    /*@Before("execution(* *.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("The method " + joinPoint.getSignature().getName()
+                + "() begins with " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("execution(* *.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        log.info("The method " + joinPoint.getSignature().getName()
+                + "() ends");
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* *.*(..))",
+            returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("The method " + joinPoint.getSignature().getName()
+                + "() ends with " + result);
+    }
+
+    @AfterThrowing(
+            pointcut = "execution(* *.*(..))",
+            throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("An exception " + e + " has been thrown in "
+                + joinPoint.getSignature().getName() + "()");
+    }
+
+    @AfterThrowing(
+            pointcut = "execution(* *.*(..))",
+            throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint,
+                                 IllegalArgumentException e) {
+        log.error("Illegal argument " + Arrays.toString(joinPoint.getArgs())
+                + " in " + joinPoint.getSignature().getName() + "()");
+    }*/
+
+    @Around("execution(* *.*(..))")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("The method " + joinPoint.getSignature().getName()
+                + "() begins with " + Arrays.toString(joinPoint.getArgs()));
+        try {
+            Object result = joinPoint.proceed();
+            log.info("The method " + joinPoint.getSignature().getName()
+                    + "() ends with " + result);
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument "
+                    + Arrays.toString(joinPoint.getArgs()) + " in "
+                    + joinPoint.getSignature().getName() + "()");
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/com/apress/springrecipes/calculator/Main.java
@@ -0,0 +1,24 @@
+package com.apress.springrecipes.calculator;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Matrix on 21.10.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("3/beans-3.1.xml");
+        ArithmeticCalculator arithmeticCalculator =
+                (ArithmeticCalculator) context.getBean("arithmeticCalculator");
+        arithmeticCalculator.add(1, 2);
+        arithmeticCalculator.sub(4, 3);
+        arithmeticCalculator.mul(2, 3);
+        arithmeticCalculator.div(4, 2);
+        UnitCalculator unitCalculator =
+                (UnitCalculator) context.getBean("unitCalculator");
+        unitCalculator.kilogramToPound(10);
+        unitCalculator.kilometerToMile(5);
+    }
+}
